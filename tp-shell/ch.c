94824935a8c080f4c89a48be0258263799f60875
payne911 <payne911@users.noreply.github.com>
2019-01-22 23:58:09 -0500

    remise init

---
+++ tp-shell/ch.c
@@ -0,0 +1,19 @@
+/* ch.c.
+auteur:
+date:
+problèmes connus:
+
+  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+  fprintf (stdout, "%% ");
+
+  /* ¡REMPLIR-ICI! : Lire les commandes de l'utilisateur et les exécuter. */
+
+  fprintf (stdout, "Bye!\n");
+  exit (0);
+}
