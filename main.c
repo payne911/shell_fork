ee62b90782cc1ac1a67c4ae56ce0878b90a16c20
payne911 <jeremi.grenier.berthiaume@gmail.com>
2019-02-05 10:46:39 -0500

    2 bytes on &

--- main.c
+++ main.c
@@ -388,12 +388,12 @@ Expression * parse_line (split_line* line, int start_index, int end_index) {
 
 void free_split_line(split_line* line) {
     /// To `free()` the char** and all its sub-arrays
-    free(line->content);
     if(DEBUG != 0) printf("free_split_line   |   line sub arrays size: %d\n", line->size);
     for(int i = 0; i < line->size; i++) {
         if(DEBUG != 0) printf("free_split_line   |   content[%d]: %s\n", i, line->content[i]);
         free(line->content[i]);
     }
+    free(line->content);
     free(line);
 }
 
