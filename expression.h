182a0cd5f5e4992371b37969716860598ee3dd96
Olivier L Applin <olivier.lepage-applin@umontreal.ca>
2019-02-04 20:12:27 -0500

    Update expression.h

--- expression.h
+++ expression.h
@@ -1,272 +1,327 @@
-//
-// Created by Olivier L.-Applin on 2019-01-25.
-//
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-#ifndef TESTSHELL_EXPRESSION_H
-#define TESTSHELL_EXPRESSION_H
+#include "expression.h"
 
-#define EXIT_SHELL "exit"
-#define LS "ls"
-#define ECHO "echo"
-#define IF_TOKEN "if"
-#define DO_TOKEN "do"
-#define DONE_TOKEN "done"
-#define OR_TOKEN "||"
-#define AND_TOKEN "&&"
-#define BACK_TOKEN "&"
-#define IF_SEP ";"
-#define REDIRECT_SEPERATOR ">"
-#define FAIL 'F'
-#define SUCCESS 'S'
-#define INVALID_INDEX -1
+#define DEFAULT_BUFFER_SIZE 64
 
 
-typedef enum {COMMAND, IF_EXPRESSION, OR_EXPRESSION, AND_EXPRESSION} exp_type;
-static char *enumStrings[] = {"COMMAND", "IF_EXPRESSION", "OR_EXPRESSION", "AND_EXPRESSION"};
-
-typedef struct command {
-    char**  cmd;
-    bool redirect_flag;
-    // bool background_flag;
-    char* output_file;
-} command;
-
-/**
- * Abstract syntax tree representation for parsing the command line.
- * Inspired by : https://lambda.uta.edu/cse5317/notes/node26.html
- */
-typedef struct Expression {
-    exp_type                                            id;
-    union {
-        command*                                        cmd_expr;
-        struct {
-            struct Expression*      left;
-            struct Expression*      right;
-        }                                               cond_expr;
-    } node;
-} Expression;
+typedef struct split_line {
+    int size;           // size of the array
+    char** content;     // array of words
+} split_line;
 
 
-Expression* create_exp      (exp_type type, Expression* first, Expression* second);
-Expression* create_cmd      (char** line, int start_index, int end_index);
+// function declaration
 
-int eval                    (Expression* exp);
-int or_eval                 (Expression* exp);
-int and_eval                (Expression* exp);
-void destroy_command        (command* cmd);
-int run_command             (command* cmd);
+split_line* split_str       (char* str, const char delim[]);
+Expression* parse_line      (split_line* line, int start_index, int end_index);
 
+size_t optimizer_cnt (const char *str) {
+    /// Used to get an upper limit on the amount of words separated by a space in the input string.
 
-/**
- * To allocate memory for an Expression
- * @param type
- * @param first
- * @param second
- * @return
- */
-Expression * create_exp (exp_type type, Expression* first, Expression* second){
+    char copied_str[strlen(str)+1];
+    strcpy(copied_str, str);  // todo: OOM ??
+    size_t count = 1;
 
-    Expression* e = malloc(sizeof(Expression));
-    if (e == NULL){
-        perror("malloc error create_exp");
-        return NULL;
+    strtok(copied_str, " ");
+    while(strtok(NULL, " ") != 0) {
+        count++;
     }
 
-    e->id = type;
-    e->node.cond_expr.left = first;
-    e->node.cond_expr.right = second;
-    return e;
-
+    return count;
 }
 
 
 /**
- *
- * @param line
+ * Splits a string into array of of strings based on all delimiters specified
+ * @param str
+ * @param delim
  * @return
- * 
  */
-Expression * create_cmd (char** line, int start_index, int end_index){
+split_line * split_str (char* str, const char delim[]) {
+    /// Returns a list of pointers to strings that came from splitting the input.
+    /// Returns NULL in case of error.
 
-    int size = end_index - start_index + 1;
-    if (strncmp(line[start_index + size - 1], IF_SEP, 1) == 0){
-        size--;
+    if (strlen(str) == 1){
+        split_line* sl = malloc(sizeof(split_line));
+        sl->size = 0;
+        sl->content = NULL;
+        return sl;
     }
 
-    Expression* e = malloc(sizeof(Expression));
-    if (e == NULL){
-        perror("malloc error create_cmd");
+    char* copied_input = strdup(str);  // because 'strtok()' alters the string it passes through
+    if (copied_input == 0) return NULL;  // OOM
+
+    /* Counting content to optimize allocated's memory size. */
+    size_t nwords = optimizer_cnt(copied_input);
+    char** result;
+    if ((result = malloc((nwords+1) * sizeof(char *))) == 0) return NULL;  // OOM
+
+    /* Obtaining first word. */
+    char* token = strtok (copied_input, delim);
+    if (token == NULL) {
+        //if(DEBUG != 0) printf("Bug extracting first word of split\n");
+        free(result);
+        free(copied_input);
+        return NULL;
+    } else if ((result[0] = strdup (token)) == 0) {  // OOM
+        //if(DEBUG != 0) printf("Bug allocating first word of split\n");
+        free(result);
+        free(copied_input);
         return NULL;
     }
 
-    e->id = COMMAND;
-
-    e->node.cmd_expr = malloc(sizeof(command));
-    if (e->node.cmd_expr == NULL){
-        free(e);
-        perror("malloc error create_cmd");
-        return NULL;
-    }
-
-
-    e->node.cmd_expr->cmd = malloc(sizeof(char*) * (size + 1));
-    e->node.cmd_expr->redirect_flag=false;
-    
-    if (e->node.cmd_expr->cmd == NULL){
-        free(e->node.cmd_expr);
-        free(e);
-        perror("malloc error create_cmd");
-        return NULL;
-    }
-
-    int i;
-    bool redir = false;
-    for(i = 0; i < size; i++){
-        if (redir){
-            char * next = line[i + start_index + 1];
-            if (strncmp(line[i+start_index], REDIRECT_SEPERATOR, 1) == 0 && 
-                (next == NULL 
-                || strncmp (next, OR_TOKEN, 2)  == 0
-                || strncmp (next, AND_TOKEN, 2) == 0
-                || strncmp (next, DO_TOKEN, 2)       == 0
-                || strncmp (next, IF_TOKEN, 2)       == 0
-                || strncmp (next, DONE_TOKEN, 4)     == 0
-                || next == IF_SEP
-                || next == REDIRECT_SEPERATOR)){
-                    printf("no output file. Redirect kept on %s", e->node.cmd_expr->output_file);
-                    return e;
+    /* To populate the whole array with the rest of the content. */
+    int tmp = 0;
+    while ((token = strtok (NULL, delim)) != NULL) {
+        if ((result[++tmp] = strdup (token)) == 0) {
+            /* In case of OOM, each past array must also be freed. */
+            while (tmp-- != 0) {  // todo: inequality is right? (will free down to result[1] inclusively?)
+                //if(DEBUG != 0) printf("Bug allocating content of split. Currently freeing index %d\n", tmp);
+                free(result[tmp]);
             }
-
-        }       
-
-        else if (strncmp(line[i+start_index], REDIRECT_SEPERATOR, 1) == 0){
-            redir = true;
-            printf("redirect found [%i]\n", i);
-            char * next = line[i + start_index + 1];
-            
-            // if next word is NULL or a token -> no redirect file
-            if (next == NULL 
-                || strncmp (next, OR_TOKEN, 2)  == 0
-                || strncmp (next, AND_TOKEN, 2) == 0
-                || strncmp (next, DO_TOKEN, 2)       == 0
-                || strncmp (next, IF_TOKEN, 2)       == 0
-                || strncmp (next, DONE_TOKEN, 4)     == 0
-                || next == IF_SEP
-                || next == REDIRECT_SEPERATOR){
-                    printf("no output file. Redirect on %s ignored\n", line[start_index]);
-                    printf("reallocating : %i\n", i + 1);
-                    char ** tmp = realloc(e->node.cmd_expr->cmd, (sizeof(char*) * (i + 1)));
-                    if (tmp == NULL){
-                        free(tmp);
-                        free(e->node.cmd_expr->cmd);
-                    }
-                    e->node.cmd_expr->cmd = tmp;
-                    e->node.cmd_expr->cmd[i+1] = NULL;
-                    e->node.cmd_expr->redirect_flag=false;
-                    return e;
-                }
-               
-                // looks like 'command > my_file.txt'
-                e->node.cmd_expr->redirect_flag=true;
-                e->node.cmd_expr->output_file=next;
-                printf("dest : %s\n", next);
-                printf("reallocating : %i\n", i + 1);
-                char ** tmp = realloc(e->node.cmd_expr->cmd, (sizeof(char*) * (i + 1)));
-                    if (tmp == NULL){
-                        free(tmp);
-                        free(e->node.cmd_expr->cmd);
-                    }
-                e->node.cmd_expr->cmd[i+1] = NULL;                
-            }
-
-        else {
-            e->node.cmd_expr->cmd[i] = line[i + start_index];
+            free(result);
+            free(copied_input);
+            return NULL;
         }
     }
 
-    e->node.cmd_expr->cmd[size] = NULL;
-    return e;
-}
+    result[++tmp] = NULL;  /* Must end with NULL */
 
+    free(copied_input);
 
-void destroy_expression (Expression* e){
-
-    printf("freeing %s\n\n", enumStrings[e->id]);
-
-    if (e==NULL){
-        free(e);
-        return;
+    if (result[0] == NULL){
+        tmp = 0;
+    }else if (str[strlen(str) - 2] == ' '){
+        tmp--;
     }
 
-    if (e->id == COMMAND){
-        destroy_command(e->node.cmd_expr);
-    }
+    // printf("Size = %d\n", tmp);
 
-    else {
-        destroy_expression(e->node.cond_expr.left);
-        destroy_expression(e->node.cond_expr.right);
-    }
-
-    free(e);
-
+    split_line* sl = malloc(sizeof(split_line));
+    sl->content = result;
+    sl->size = tmp;
+    return sl;
 }
 
-
-void destroy_command(command* cmd_expr){
-    free(cmd_expr->cmd);
-    free(cmd_expr);
-}
 
 /**
- * evaluation of an expression
- *
- * @param exp the expression to evaluate
- * @return if the evaluation succeeded or not
+ * run a command line
+ * @param command
+ * @return
  */
-int eval (Expression* exp){
-    printf("evaluation : %s\n", enumStrings[exp->id]);
-    switch (exp->id){
-        case IF_EXPRESSION :
-            if (eval(exp->node.cond_expr.left)){
-                return eval(exp->node.cond_expr.right);
-            } else {
-                return 0; // failure
+int run_command(command * cmd){
+    
+    printf("\t%s\n", cmd->cmd[0]);
+    if (cmd->redirect_flag) printf("\nREDIRECT TO %s", cmd->output_file);
+    // if (cmd->background_flag) printf("DO IN BACKGROUND");
+    return 1;
+}
+
+
+/**
+ * from a list of words, parse it to create a abstract syntax tree
+ *
+ * @param line
+ * @param start_index
+ * @param end_index
+ * @return
+ */
+Expression * parse_line (split_line* line, int start_index, int end_index){
+
+    //printf("parsing line from %d to %d\n", start_index, end_index);
+
+    int if_count = 0;
+    int do_count = 0;
+    int done_count = 0;
+
+    // look for the root of the AST : either the first if statement,
+    // or the first logical operator outside an if statement
+    for (int i = start_index; i < end_index; ++i) {
+
+        char* current = line->content[i];
+
+        if (strncmp(current, IF_TOKEN, 2) == 0){
+            if_count++;
+        }
+
+        if (strncmp(current, DO_TOKEN, 2) == 0 && strncmp(current, DONE_TOKEN, 4) != 0){
+            do_count++;
+        }
+
+        if (strncmp(current, DONE_TOKEN, 4) == 0){
+            done_count++;
+        }
+
+        // outside of an if statement implies if_count == done_count
+
+        // the 'and' token at 'i' is the root
+        if (strncmp(current, AND_TOKEN, 3) == 0 && if_count == done_count){
+            //printf("the \'AND\' token at %d is the root\n", i);
+            Expression* left = parse_line(line, start_index, i - 1);
+            if (left == NULL){
+                return NULL;
             }
-        case COMMAND:            return run_command(exp->node.cmd_expr);
-        case OR_EXPRESSION:      return or_eval(exp);
-        case AND_EXPRESSION:     return and_eval(exp);
-        default:                 return -1;
+            Expression* right = parse_line(line, i+1, end_index);
+            if (right == NULL){
+                destroy_expression(left);
+                return NULL;
+            } else {
+                return create_exp(AND_EXPRESSION, left, right);
+            }
+        }
+
+        // the 'or' token at 'i' is the root
+        if (strncmp(current, OR_TOKEN, 2) == 0 && if_count == done_count){
+            //printf("the \'OR\' token at %d is the root\n", i);
+            Expression* left = parse_line(line, start_index, i - 1);
+            if (left == NULL){
+                return NULL;
+            }
+            Expression* right = parse_line(line, i+1, end_index);
+            if (right == NULL){
+                destroy_expression(left);
+                return NULL;
+            } else {
+                return create_exp(OR_EXPRESSION, left, right);
+            }
+
+        }
+
     }
+
+    // error if statement
+    if ((if_count != 0 || do_count != 0 || done_count != 0) && ((if_count != do_count) && (if_count != done_count))){
+        printf("error in parsing if statement (if:%d, do:%d, done:%d)\n", if_count, do_count, done_count);
+        return NULL;
+    }
+
+    // here, the root of the AST is the 'if' statement at the beginning of the
+    if (if_count != 0 && if_count == done_count){
+        //printf("the \'IF\' token at %d is the root\n", start_index);
+
+        // inside an if statement.
+        // Need to separate the 'if' and 'do' statements
+        if_count = 0;
+        do_count = 0;
+        done_count = 0;
+
+        int do_index = INVALID_INDEX;
+        int done_index = INVALID_INDEX;
+        bool do_found = false;
+
+        for (int j = start_index; j < end_index; ++j){
+
+            char* current = line->content[j];
+
+            if (strncmp(current, IF_TOKEN, 2) == 0){
+                if_count++;
+            }
+
+            if (strncmp(current, DO_TOKEN, 2) == 0){
+                do_count++;
+            }
+
+            if (strncmp(current, DONE_TOKEN, 4) == 0){
+                done_count++;
+            }
+
+            if (if_count == do_count && !do_found){
+                do_index = j;
+                do_found = true;
+            }
+
+            if (if_count == done_count){
+                done_index = j;
+            }
+
+        }
+
+        //printf("if : %d, do : %d\n", start_index, do_index);
+
+        // if :
+        Expression * condition = parse_line(line, start_index + 1, do_index - 1);
+        if (condition==NULL){
+            return NULL;
+        }
+
+        Expression * statement = parse_line(line, do_index +1, done_index - 1);
+        if (statement == NULL){
+            destroy_expression(condition);
+            return NULL;
+        }
+
+        return create_exp(IF_EXPRESSION, condition, statement);
+    }
+
+    // there is no 'if' statement, nor logical operator. The commnad line is a single command.
+    if (if_count == 0){
+        return create_cmd(line->content, start_index, end_index);
+    }
+
+    // error in parsing
+    printf("error in parsing\n");
+    return NULL;
+
 }
 
 
-/**
- * if left_statement works, do not run right_statement. Run right_statement only of left statement works.
- * @param exp
- * @return
- */
-int or_eval (Expression* exp){
-    int first_result = eval(exp->node.cond_expr.left);
-    if (first_result){
-        return first_result;
-    } else {
-        return eval(exp->node.cond_expr.right);
-    }
+
+int main() {
+
+    // system("clear");
+
+    // read line continuously
+    do {
+
+        char* read_buffer = malloc(sizeof(char) * DEFAULT_BUFFER_SIZE);
+        int buffer_size = DEFAULT_BUFFER_SIZE;
+
+        int index = 0;
+        char ch;
+
+        printf("votre shell> ");
+        do {
+            scanf("%c", &ch);
+            read_buffer[index++] = ch;
+            if (index == buffer_size){
+                read_buffer = realloc(read_buffer, (size_t)index+DEFAULT_BUFFER_SIZE);
+                buffer_size = index+DEFAULT_BUFFER_SIZE;
+            }
+
+        } while(ch != '\n');
+
+        // end string
+        read_buffer[index] = '\0';
+
+        if (strncmp(read_buffer, EXIT_SHELL, 4) == 0){
+            free(read_buffer);
+            break;
+        }
+
+        // here, read_buffer is the whole line
+        split_line* line = split_str(read_buffer, " ");
+        free(read_buffer);
+
+        // no input
+        if (line->size == 0){
+            // printf("no input\n");
+            continue;
+        }
+        Expression* ast = parse_line(line, 0, line->size - 1);
+        //__debug_print(ast, 0);
+
+        eval(ast);
+
+        // once the line has been used, free memory
+        destroy_expression(ast);
+        free(line->content);
+        free(line);
+
+    } while (true);
+
+    return 0;
 }
-
-
-/**
- *
- * @param exp
- * @return
- */
-int and_eval (Expression* exp){
-    int first_result = eval(exp->node.cond_expr.left);
-    if (first_result){
-        return eval(exp->node.cond_expr.right);
-    } else {
-        return false;
-    }
-}
-
-#endif //TESTSHELL_EXPRESSION_H
