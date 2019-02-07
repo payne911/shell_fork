585678c342e45721d46a151b871e44eaf6e83699
payne911 <jeremi.grenier.berthiaume@gmail.com>
2019-02-07 16:15:56 -0500

    reorganization

--- main.c
+++ main.c
@@ -41,6 +41,69 @@ problèmes connus:
 volatile pid_t CURR_CHILD = 0;
 
 
+/*
+ * Function declarations.
+ */
+
+Split_line* form_split_line(char**, int);
+char** query_and_split_input();
+size_t optimizer_cnt(const char*);
+int count_words(char**);
+char** split_str (const char*, const char[]);
+void debug_free(char**);
+void set_up_handlers();
+void z_handler();
+void ctrl_Z_handler(int);
+void remove_Z_handler();
+void set_up_reaper();
+void zombie_handler(int);
+void run_bg_cmd(Split_line*);
+bool writeOutputInFile(const char*);
+int run_command(Command*);
+void run_fg_cmd(Split_line*);
+
+
+/*
+ * Utils.
+ */
+
+Split_line* form_split_line(char** args, int wordc) {
+    /// Properly sets up a `Split_line` with the char** obtained from query.
+
+    Split_line* sl = malloc(sizeof(Split_line));
+    if(sl == NULL) return NULL;  // OOM
+    sl->content = args;
+    sl->size = wordc;
+    sl->thread_flag = ((strcmp(args[wordc-1], "&")) == 0); // find trailing '&'
+    return sl;
+}
+
+char** query_and_split_input() {
+    /// Asks for a command. Ignores anything beyond first '\n'.
+    /// Returns the array resulting from splitting the input.
+
+    /* Prompting for command. */
+    printf ("\nshell> ");  // `\n` to ensure the printf-buffer is emptied
+
+    char* input_str = NULL;
+    size_t buffer_size;
+    if (getline(&input_str, &buffer_size, stdin) == -1) {
+        free(input_str);
+        return NULL;
+    }
+    input_str[strcspn(input_str, "\n")] = 0;  // crop to first new-line
+
+    /* Splitting the input string. */
+    char** args;
+    if ((args = split_str (input_str, " ")) == 0) {
+        free(input_str);
+        return query_and_split_input();
+    } else {
+        free(input_str);
+        return args;
+    }
+}
+
 size_t optimizer_cnt(const char *str) {
     /// Returns min(1, #words).
 
@@ -113,7 +176,121 @@ char** split_str (const char* str, const char delim[]) {
     return result;
 }
 
+
+/*
+ * Memory management.
+ */
+
+void debug_free(char** args) {
+    /// Lazy function to ensure `args` is freed properly.
+
+    int count = count_words(args);
+    Split_line* line = form_split_line(args, count);
+    free_split_line(line);
+}
+
+
+/*
+ * Bonus 2
+ */
+
+void set_up_handlers() {
+    /// To initialize the signal handlers.
+
+    set_up_reaper();
+    z_handler();
+}
+
+void z_handler() {
+    /// To treat Ctrl-Z (Bonus #2).
+
+    struct sigaction bonus2_signal;
+    bonus2_signal.sa_handler = &ctrl_Z_handler;
+    sigemptyset(&bonus2_signal.sa_mask);
+    bonus2_signal.sa_flags = SA_RESTART;
+    sigaction(SIGTSTP, &bonus2_signal, NULL);
+}
+
+void ctrl_Z_handler(int sigNo) {
+    /// Stops the process identified by the CURR_CHILD pid.
+
+    if(CURR_CHILD != 0)  // prevents crash if there wasn't any child created yet
+        kill(CURR_CHILD, SIGSTOP);  // triggers the waitpid in the parent
+}
+
+void remove_Z_handler() {
+    /// Used to ensure a child-process will not detect the Ctrl-Z.
+
+    struct sigaction signal_ignore;
+    signal_ignore.sa_handler = SIG_IGN;
+    sigemptyset(&signal_ignore.sa_mask);
+    signal_ignore.sa_flags = 0;
+    sigaction(SIGTSTP, &signal_ignore, NULL);
+}
+
+
+/*
+ * Q2.4
+ */
+
+void set_up_reaper() {
+    /// To reap zombie-child processes.
+
+    struct sigaction zombie_reaper;
+    zombie_reaper.sa_handler = &zombie_handler;
+    sigemptyset(&zombie_reaper.sa_mask);
+    zombie_reaper.sa_flags = SA_RESTART | SA_NOCLDSTOP;
+    sigaction(SIGCHLD, &zombie_reaper, NULL);
+}
+
+void zombie_handler(int sigNo) {
+    /// Reaps the processes it receives signals from.
+
+    int status;
+    while ((waitpid(-1, &status, WNOHANG)) > 0)
+        ;  // always reap children
+}
+
+void run_bg_cmd(Split_line *line) {
+    /// To execute a chain of command(s) in the background.
+
+    /* To replace the trailing '&' from the command. */
+    free(line->content[line->size-1]);
+    line->content[line->size-1] = NULL;
+    Expression* ast = parse_line(line, 0, line->size - 2);
+
+    /* Forking. */
+    pid_t    pid;
+    pid = fork();
+
+    if (pid < 0) {  // error while forking
+        free_split_line(line);
+
+    } else if (pid == 0) {  // child
+        remove_Z_handler();  // prevent Cltr-Z triggers in child
+
+        // todo: if "cat" or "vi", stop process ? (bonus 1)
+
+        /* Executing the command(s). */
+        eval(ast);
+
+        /* Child process failed. */
+        free_split_line(line);
+        destroy_expression(ast);
+        exit(-1);
+
+
+    } else {  // back in parent
+
+        /* Freeing memory. */
+        free_split_line(line);
+        destroy_expression(ast);
+    }
+}
+
 bool writeOutputInFile(const char* output) {
+    /// Redirecting `stdout` to a file-descriptor.
+
     int fileDescriptor = open(output,O_WRONLY|O_CREAT, S_IRWXU|S_IRWXG|S_IRWXO);
     if (fileDescriptor < 0)         return false;    // in case of error
     if (dup2(fileDescriptor,1) < 0) return false;    // in case of error
@@ -121,6 +298,10 @@ bool writeOutputInFile(const char* output) {
     return true;
 }
 
+
+/*
+ * Q2.1
+ */
 
 int run_command(Command* cmd) {
     /// Called throughout the recursion that evaluates every single command.
@@ -133,7 +314,7 @@ int run_command(Command* cmd) {
         return false;
 
     } else if (CURR_CHILD == 0) {  // child
-        signal(SIGTSTP, SIG_IGN);  // prevent Cltr-Z triggers in child
+        remove_Z_handler();  // prevent Cltr-Z triggers in child
 
         /* Redirecting output. */
         if(cmd->redirect_flag) {
@@ -165,7 +346,6 @@ int run_command(Command* cmd) {
     }
 }
 
-
 void run_fg_cmd(Split_line *line) {
     /// To execute a chain of command(s) in the foreground.
 
@@ -177,123 +357,6 @@ void run_fg_cmd(Split_line *line) {
     destroy_expression(ast_root);
     free_split_line(line);
 }
-
-void run_bg_cmd(Split_line *line) {
-    /// To execute a chain of command(s) in the background.
-
-    /* To replace the trailing '&' from the command. */
-    free(line->content[line->size-1]);
-    line->content[line->size-1] = NULL;
-    Expression* ast = parse_line(line, 0, line->size - 2);
-
-    /* Forking. */
-    pid_t    pid;
-    pid = fork();
-
-    if (pid < 0) {  // error while forking
-        free_split_line(line);
-
-    } else if (pid == 0) {  // child
-        signal(SIGTSTP, SIG_IGN);  // prevent Cltr-Z triggers in child
-
-        // todo: if "cat" or "vi", stop process ? (bonus 1)
-
-        /* Executing the command(s). */
-        eval(ast);
-
-        /* Child process failed. */
-        free_split_line(line);
-        destroy_expression(ast);
-        exit(-1);
-
-
-    } else {  // back in parent
-
-        /* Freeing memory. */
-        free_split_line(line);
-        destroy_expression(ast);
-    }
-}
-
-Split_line* form_split_line(char** args, int wordc) {
-    /// Properly sets up a `Split_line` with the char** obtained from query.
-
-    Split_line* sl = malloc(sizeof(Split_line));
-    if(sl == NULL) return NULL;  // OOM
-    sl->content = args;
-    sl->size = wordc;
-    sl->thread_flag = ((strcmp(args[wordc-1], "&")) == 0); // find trailing '&'
-    return sl;
-}
-
-char** query_and_split_input() {
-    /// Asks for a command. Ignores anything beyond first '\n'.
-    /// Returns the array resulting from splitting the input.
-
-    /* Prompting for command. */
-    printf ("\nshell> ");  // `\n` to ensure the printf-buffer is emptied
-
-    char* input_str = NULL;
-    size_t buffer_size;
-    if (getline(&input_str, &buffer_size, stdin) == -1) {
-        free(input_str);
-        return NULL;
-    }
-    input_str[strcspn(input_str, "\n")] = 0;  // crop to first new-line
-
-    /* Splitting the input string. */
-    char** args;
-    if ((args = split_str (input_str, " ")) == 0) {
-        free(input_str);
-        return query_and_split_input();
-    } else {
-        free(input_str);
-        return args;
-    }
-}
-
-
-void zombie_handler(int sigNo) {
-    /// Reaps the processes it receives signals from.
-
-    int status;
-    while ((waitpid(-1, &status, WNOHANG)) > 0)
-        ;  // always reap children
-}
-
-void ctrl_Z_handler(int sigNo) {
-    /// Stops the process identified by the CURR_CHILD pid.
-
-    if(CURR_CHILD != 0)  // prevents crash if there wasn't any child created yet
-        kill(CURR_CHILD, SIGSTOP);  // triggers the waitpid in the parent
-}
-
-void debug_free(char** args) {
-    /// Lazy function to ensure `args` is freed properly.
-
-    int count = count_words(args);
-    Split_line* line = form_split_line(args, count);
-    free_split_line(line);
-}
-
-void set_up_handlers() {
-    /// To set up the signal handlers.
-
-    /* To reap zombie-child processes. */
-    struct sigaction zombie_reaper;
-    zombie_reaper.sa_handler = &zombie_handler;
-    sigemptyset(&zombie_reaper.sa_mask);
-    zombie_reaper.sa_flags = SA_RESTART | SA_NOCLDSTOP;
-    sigaction(SIGCHLD, &zombie_reaper, NULL);
-
-    /* To treat Ctrl-Z (Bonus #2). */
-    struct sigaction bonus2_signal;
-    bonus2_signal.sa_handler = &ctrl_Z_handler;
-    sigemptyset(&bonus2_signal.sa_mask);
-    bonus2_signal.sa_flags = SA_RESTART;
-    sigaction(SIGTSTP, &bonus2_signal, NULL);
-}
-
 
 int main (void) {
     /// Instanciates the main shell and queries the command(s).
